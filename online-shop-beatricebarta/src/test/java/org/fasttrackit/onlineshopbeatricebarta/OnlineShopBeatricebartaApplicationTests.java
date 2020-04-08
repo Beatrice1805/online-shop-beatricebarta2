dd9dcabdcd9061e70f3cf9e8e514cdf2559874e4
Beatrice <oprea.beatrice@yahoo.com>
2020-04-08 17:08:20 +0300

    Initial commit(project structure)

---
+++ online-shop-beatricebarta/src/test/java/org/fasttrackit/onlineshopbeatricebarta/OnlineShopBeatricebartaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fasttrackit.onlineshopbeatricebarta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineShopBeatricebartaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
